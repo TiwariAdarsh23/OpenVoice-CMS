c18db4dd8c5b912f3ff04821630cdd8fa9270b2a
Adarsh_Tiwari <pankajadarsh2311@gmail.com>
2025-12-07 15:45:47 +0530

    Frontend app.jsx

---
+++ client/src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='text-3xl font-bold text-center mt-20 text-primary'>
+       OpenVoice Setup Complete
+    </div>
+  )
+}
+export default App
