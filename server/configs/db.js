7b753d858ab37577310d8a778d9eddaca81c1260
Adarsh_Tiwari <pankajadarsh2311@gmail.com>
2025-12-12 23:09:53 +0530

    Backend-Configured MongoDB connection

---
+++ server/configs/db.js
@@ -0,0 +1,10 @@
+import mongoose, { connect } from "mongoose";
+const connectDB=async () => {
+    try{
+        mongoose.connection.on('connected',()=>console.log("Database Connected"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/blogpost`)
+    }catch(error){
+        console.log(error.message);
+    }
+}
+export default connectDB;
