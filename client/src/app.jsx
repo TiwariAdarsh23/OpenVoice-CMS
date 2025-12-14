dbee3929c47237e758ce26b25be24f06e5f87bc8
Adarsh_Tiwari <pankajadarsh2311@gmail.com>
2025-12-14 15:49:59 +0530

    final modifications

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,18 +2,37 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
-import CreatePost from './pages/CreatePost'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
+import Login from './components/admin/Login'
+import CreatePost from './pages/CreatePost' // We will create this next
 import 'quill/dist/quill.snow.css'
+import { Toaster } from 'react-hot-toast'
 import { useAppContext } from './context/AppContext'
+
 const App = () => {
   const { token } = useAppContext();
   
   return (
     <div>
+      <Toaster/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/blog/:id' element={<Blog/>} />
+        
+        {/* New Route for Community Writers */}
         <Route path='/create-post' element={<CreatePost/>} />
+
+        {/* Existing Admin Routes (Protected by your Custom Token) */}
+        <Route path='/admin' element={token ? <Layout/> : <Login/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='addBlog' element={<AddBlog/>}/>
+          <Route path='listblog' element={<ListBlog/>}/>
+          <Route path='comments' element={<Comments/>}/>
+        </Route>
       </Routes>
     </div>
   )
